8b49864309cedf26b5bc6cea57addf98bba4705a
Alexandru Tatarciuc <tatarciucalexandru@gmail.com>
2023-07-28 16:11:14 +0200

    normal ass formatter + improved navigation

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,7 +3,8 @@
     export let isFixed : boolean = false;
 </script>
 
-<nav class="{isFixed ? 'fixed' : 'absolute' } top-0 left-1/2 transform -translate-x-1/2 flex items-center gap-auto lg:gap-6 justify-between max-w-sm lg:max-w-lg my-4 mx-auto py-2 px-4 sm:px-6 border-2 border-gray-900 rounded-full">
+<nav class="{isFixed ? 'fixed' : 'absolute' } top-0 left-1/2 transform -translate-x-1/2 flex items-center gap-auto lg:gap-6 justify-between max-w-sm lg:max-w-lg  my-4 mx-auto py-2 px-4 sm:px-6 rounded-full">
+    <span class="absolute -inset-[2px] border-2 border-gray-900 rounded-full backdrop-blur-lg"></span>
     <NavItem href="/" label="Projects" />
     <NavItem href="/cv" label="CV" />
     <NavItem href="/about" label="About Me" />
--- src/lib/NavItem.svelte
+++ src/lib/NavItem.svelte
@@ -11,7 +11,7 @@
     }
   </script>
   
-  <a class:bg-primary={isActive} class="py-4 px-6 font-display text-sm lg:text-xl font-medium whitespace-nowrap rounded-full text-center min-w-0 sm:min-w-[6rem] lg:min-w-[8rem]" href="#" on:click|preventDefault={navigate}>{label}</a>
+  <a class:bg-primary={isActive} class="z-20 py-4 px-6 font-display text-sm lg:text-xl font-medium whitespace-nowrap rounded-full text-center min-w-0 sm:min-w-[6rem] lg:min-w-[8rem]" {href}>{label}</a>
   <!-- <a
   class:active={$page.url.pathname.split("/")[1] ===item.href.split("/")[1]}
   href={item.href}>
